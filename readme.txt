95bf161345ca29269094a51713c60d950c4a887b
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-03-16 15:00:35 -0400

    Removes trailing whitespaces, replaces tabs

--- readme.txt
+++ readme.txt
@@ -29,24 +29,24 @@ Known Issues
 - Thread Exceptions:
 A thread exception in one thread does not exit the entire evaluation in a Virutal Namespace.  So far exceptions in a thread are caught and logged but the execution of other healthy threads continues.
 - TCP/UDP listeners are not isolated
-A listener registered on one IP:PORT will override another listener previously registered on this IP:PORT 
- 
-############   
+A listener registered on one IP:PORT will override another listener previously registered on this IP:PORT
+
+############
 Links
 
 https://seattle.cs.washington.edu/wiki
 https://seattle.cs.washington.edu/wiki/ProgrammersPage
 https://seattle.cs.washington.edu/wiki/RepyApi
 
-############   
+############
 Description
 
 - Editor
 The editor is always displayed on the left side. You can write Repy code to it  and evaluate it on the server. To submit the code press cmd + return (mac), ctrl + return (win), or the submit button beneath the editor window. Until the code has been evaluated, codesubmission is locked.
-  
+
 - Callargs
 You can append space separated callarguments in the input line beneath the  editor window, when submitting code.
-  
+
 - Standard Output
 This displays the output of the program. It gives "real time" feedback.
 
@@ -54,7 +54,7 @@ This displays the output of the program. It gives "real time" feedback.
 This retrieves and displays the entire log for every submitted and evaluated code of a session
 
 - Insert Files
-Insert files to the editor window, whether at cursor position or at the top of the editor window. 
+Insert files to the editor window, whether at cursor position or at the top of the editor window.
 
 - Special Characters
 Insert special characters at cursor position.
@@ -67,7 +67,7 @@ Options to customize the editor window.
 
 - Read Me
 this.
-  
+
 Todo
 
 - Network Ressource scheduling
--- web/js/tr_repy.js
+++ web/js/tr_repy.js
@@ -13,10 +13,10 @@
   This is the the only non-ace java script file in Try Repy.
   It initializes the ace editor.
   It reloads ace themes, to accelerate first loading.
-  It provides ajax transmission to request the log, submit the code and 
+  It provides ajax transmission to request the log, submit the code and
   receive the output.
   Additionally it controls the navigiation menu (toggle_bar) and provides
-  possibility to insert files to the editor. 
+  possibility to insert files to the editor.
 
 *****************************************************************/
 
@@ -35,33 +35,33 @@ function init_ace() {
       Sets the theme to "textmate" and links (loads) other themes for
       user customization.
       Sets the SyntaxHighlighting to Python.
-      
+
       Makes special "insert_elements" (special characters, code snippets)
       clickable and insertable.
-      
+
       Registers key command to submit code.
-      
+
   *****************************************************************/
   g_editor = ace.edit("editor");
-  
+
   /* Set initial theme and load other themes */
   g_editor.setTheme("ace/theme/textmate");
   load_themes();
-  
+
   /* Syntaxhighlighting */
   var PythonMode = require("ace/mode/python").Mode;
   g_editor.getSession().setMode(new PythonMode());
-    
+
   g_editor.renderer.setHScrollBarAlwaysVisible(false);
   g_editor.setShowPrintMargin(false);
-    
-  /* Adds onClick insert to editor function 
+
+  /* Adds onClick insert to editor function
      to all elements of class insert_element */
   make_clickable("g_editor.insert(this.innerHTML)", "insert_element");
-  
+
   /* Stores submit code button to disable later during code evaluation. */
   g_submit_button = document.getElementById("submit_button");
-  
+
   g_canon = require("pilot/canon");
   register_submit_key();
 }
@@ -72,13 +72,13 @@ function load_themes() {
     <Purpose>
       In order to accelerate initial site loading, themes are loaded
       dynamically after the site is already visible/useable.
-  
+
   *****************************************************************/
   themelist = ["clouds_midnight", "clouds", "cobalt", "crimson_editor",
     "dawn", "eclipse", "idle_fingers", "kr_theme", "merbivore_soft","merbivore",
     "mono_industrial", "monokai", "pastel_on_dark", "solarized_dark",
     "solarized_light", "twilight", "vibrant_ink" ];
-    
+
   for (var i in themelist) {
     var js = document.createElement('script');
     js.setAttribute("charset", "utf-8", 1);
@@ -94,8 +94,8 @@ function register_submit_key() {
     <Purpose>
       Registers a key command for code submission.
       Enables the code submit button.
-      Sets editor readonly false. 
-      
+      Sets editor readonly false.
+
   *****************************************************************/
   g_canon.addCommand({
     name: "submitCodeKey",
@@ -105,11 +105,11 @@ function register_submit_key() {
       call_server_evaluate(env, args, request);
       toggle("output_toggle", "output_container");
     }
-  }); 
-  
+  });
+
   g_submit_button.disabled = false;
   g_editor.setReadOnly(false);
-  
+
 }
 
 
@@ -117,10 +117,10 @@ function register_submit_key() {
 function deregister_submit_key() {
   /*****************************************************************
     <Purpose>
-      Deregisters a key command for code submission. Sets editor 
-      readonly true. This is done while submitted code is evaluated, 
+      Deregisters a key command for code submission. Sets editor
+      readonly true. This is done while submitted code is evaluated,
       to prohibit another interfering code submission.
-      
+
   *****************************************************************/
   g_submit_button.disabled = true;
   g_editor.setReadOnly(true);
@@ -133,29 +133,29 @@ function create_xml_http() {
   /*****************************************************************
     <Purpose>
       Creates an XmlHttp (ajax) object.
-    
+
     <Returns>
       XmlHttp object (java script object)
-  *****************************************************************/  
+  *****************************************************************/
   var xmlHttp = false;
-  
-  try { 
-    /* try the first version of JavaScript in IE */ 
+
+  try {
+    /* try the first version of JavaScript in IE */
     xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
   } catch (e) {
     try {
-      /* try the second version of JavaScript in IE */ 
+      /* try the second version of JavaScript in IE */
       xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
     } catch (e2) {
       xmlHttp = false;
     }
   }
 
-  /* else create the object the non-Microsoft way. */ 
+  /* else create the object the non-Microsoft way. */
   if (!xmlHttp && typeof XMLHttpRequest != "undefined") {
     xmlHttp = new XMLHttpRequest();
   }
-  
+
   return xmlHttp;
 }
 
@@ -169,39 +169,39 @@ function call_server_log() {
       empty, it means the webinterface has no sandbox on the server.
       If the response is an empty JSON object, the sandbox exists, but
       nothing has been logged yet.
-  
-  *****************************************************************/  
+
+  *****************************************************************/
   g_xmlHttp_log = create_xml_http();
-  
+
   var url = "getLog?" + g_user_id;
   g_xmlHttp_log.open("GET", url, true);
-  g_xmlHttp_log.setRequestHeader("Content-Type", 
+  g_xmlHttp_log.setRequestHeader("Content-Type",
    "application/x-www-form-urlencoded");
-   
+
   g_xmlHttp_log.onreadystatechange = function() {
     if (this.readyState == 4) {
-      
+
       // Convert string to JSON Object
       var response = this.responseText;
-      
-      
+
+
       var log_element =  document.getElementById("log");
       log_element.innerHTML = ""
-      
-      if (response == "{'entire_log' : [  ] }") { 
+
+      if (response == "{'entire_log' : [  ] }") {
         log_element.innerHTML = "<pre>(nothing logged yet)</pre>";
       }
       else if (response == "") {
         log_element.innerHTML =
         "<pre>You currently have no sandbox. Reload website!</pre>";
-      } 
+      }
       else {
         var responseJson = eval ("(" + response + ")");
 
         for (var j = 0; j < responseJson.entire_log.length; j++ ) {
           log_element.innerHTML += "-----------------------------------<br/>"
           log_element.innerHTML += "Logtime: ";
-          log_element.innerHTML += "<pre>" + 
+          log_element.innerHTML += "<pre>" +
             responseJson.entire_log[j].logtime + "</pre>";
           log_element.innerHTML += "Repycode: ";
           log_element.innerHTML += "<pre>"
@@ -211,9 +211,9 @@ function call_server_log() {
           log_element.innerHTML += "<pre>"
             + htmlEntities(unescape(responseJson.entire_log[j].output) )
             + "</pre>";
-          
+
         }
-      } 
+      }
     }
   }
   g_xmlHttp_log.send(null);
@@ -223,8 +223,8 @@ function htmlEntities(str) {
   /*****************************************************************
     <Purpose>
       Converts characters to HTML Entities.
-      
-  *****************************************************************/  
+
+  *****************************************************************/
   return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
     .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
@@ -236,38 +236,38 @@ function call_server_evaluate(env, args, request) {
      Posts submitted code + call arguments via ajax. Response will be
      the evaluated codes ouput.
      Additionally it calls call_server_output.
-     Upon receiving the response, it posts the repsonse to the output 
+     Upon receiving the response, it posts the repsonse to the output
      element.
 
   *****************************************************************/
   g_xmlHttp_evaluate = create_xml_http();
-  
+
   var code = g_editor.getSession().getValue();
   var user_callargs = document.getElementById("user_callargs").value;
-  var input = "user_code=" + escape(code) +"&user_callargs=" + 
+  var input = "user_code=" + escape(code) +"&user_callargs=" +
    escape(user_callargs);
 
   /* Make a connection to the server  */
   g_xmlHttp_evaluate.open("POST", "/?" + g_user_id, true);
-  g_xmlHttp_evaluate.setRequestHeader("Content-Type", 
+  g_xmlHttp_evaluate.setRequestHeader("Content-Type",
     "application/x-www-form-urlencoded");
-  
+
   var output = document.getElementById("output");
   output.innerHTML = "";
 
   /* Setup a function for the server to run when it's done*/
-  g_xmlHttp_evaluate.onreadystatechange = function() {   
+  g_xmlHttp_evaluate.onreadystatechange = function() {
     if (this.readyState == 4) {
       var response = this.responseText;
       output.innerHTML += htmlEntities(unescape(response));
       register_submit_key();
     }
   }
-  
-  // Send the request 
+
+  // Send the request
   g_xmlHttp_evaluate.send(input);
   call_server_buffer();
-  
+
 }
 
 function call_server_buffer() {
@@ -281,14 +281,14 @@ function call_server_buffer() {
 
   var url = "getOutput?" + g_user_id;
   g_xmlHttp_outputbuffer.open("GET", url, true);
-  g_xmlHttp_outputbuffer.setRequestHeader("Content-Type", 
+  g_xmlHttp_outputbuffer.setRequestHeader("Content-Type",
    "application/x-www-form-urlencoded");
-   
+
   var output = document.getElementById("output");
-   
+
   g_xmlHttp_outputbuffer.onreadystatechange = function() {
     if (this.readyState == 4) {
-      
+
       var response = this.responseText;
       output.innerHTML += htmlEntities(unescape(response));
 
@@ -304,9 +304,9 @@ function call_server_buffer() {
 function make_clickable(function_str, class_name) {
   /*****************************************************************
     <Purpose>
-     Assigns OnClick function <function_str> to an HTML element with 
-     class attribute <class_name>. 
-  
+     Assigns OnClick function <function_str> to an HTML element with
+     class attribute <class_name>.
+
     <Arguments>
       function_str (string)
         A string, containing the function + arguments (how it is called).
@@ -326,7 +326,7 @@ function toggle(click_class_name, toggle_class_name) {
     <Purpose>
      Toggles various right_box contents.
      Changes style of navigation bar.
-  
+
     <Arguments>
       click_class (string)
         The class name of the navigation (clicked) element.
@@ -334,7 +334,7 @@ function toggle(click_class_name, toggle_class_name) {
         The class name of the element which should be toggled.
 
   *****************************************************************/
-  
+
   var click_element = document.getElementById(click_class_name);
   var click_elements = document.getElementsByClassName("click_element");
   for (var i = 0; i < click_elements.length; i++) {
@@ -343,7 +343,7 @@ function toggle(click_class_name, toggle_class_name) {
    }
   click_element.style.color = "white";
   click_element.style.background = "black";
-  
+
   var toggle_element = document.getElementById(toggle_class_name);
   var toggle_elements = document.getElementsByClassName("toggle_element");
   for (var i = 0; i < toggle_elements.length; i++) {
@@ -359,11 +359,11 @@ function remove_file_input_row() {
       Removes the last input file row in Try Repy's insert file section.
 
   *****************************************************************/
-    
+
   var table_element = document.getElementById("file_input_table");
   var tr_count = document.getElementsByName("file_input").length;
   var tr_element = document.getElementById(("input_row" + tr_count));
-  
+
   table_element.removeChild(tr_element);
 }
 
@@ -374,20 +374,20 @@ function append_file_input_row() {
       Appends input file row in Try Repy's insert file section.
 
   *****************************************************************/
-  
+
   var table_element = document.getElementById("file_input_table");
   var tr_count = document.getElementsByName("file_input").length;
 
   var tr_element = document.createElement("tr");
   tr_element.id = "input_row" + (tr_count + 1);
-  
+
   var td_element = document.createElement("td");
-  
+
   var file_element = document.createElement("input");
   file_element.type = "file";
   file_element.name = "file_input";
   file_element.accept = "text/*";
-  
+
   td_element.appendChild(file_element);
   tr_element.appendChild(td_element);
   table_element.appendChild(tr_element);
@@ -398,93 +398,93 @@ function process_file_form() {
   /*****************************************************************
     <Purpose>
       Reads in input elements from HTML Form Data in order to insert
-      locally stored files to the editor window. 
+      locally stored files to the editor window.
 
   *****************************************************************/
-  
+
   var insert_mode;
   var insert_mode_len = document.file_form.file_insert_mode.length;
-  
+
   for (i = 0; i < insert_mode_len; i++) {
     if ( document.file_form.file_insert_mode[i].checked ) {
       insert_mode = document.file_form.file_insert_mode[i].value;
       break;
     }
   }
-  
+
   var delim_mode;
   var delim_mode_len = document.file_form.file_delim_mode.length;
-  
+
   for (j = 0; j < delim_mode_len; j++) {
     if ( document.file_form.file_delim_mode[j].checked ) {
       delim_mode = document.file_form.file_delim_mode[j].value;
       break;
     }
   }
-  
+
   var files = document.getElementsByName("file_input");
   var files_len = document.getElementsByName("file_input").length;
-  
+
   for (k = 0; k < files_len; k++) {
     file = files[k].files[0];
     read_file(file, insert_mode, delim_mode);
   }
-  
+
 }
 
 
 function read_file(file, insert_mode, delim_mode) {
   /*****************************************************************
     <Purpose>
-      Helper function that creates a FileReader for the passed 
+      Helper function that creates a FileReader for the passed
       file_object. It reads the data and creates a callback function
-      for when the file is read. 
-      
+      for when the file is read.
+
     <Arguments>
       file
         HTML [file object]
-        
-      insert_mode  (string) 
+
+      insert_mode  (string)
         "begin" or "cursor" - specifies where exactly in the editor window the
         file should be inserted
-      
+
       delim_mode (string)
         "lines", "line" or "none" - specifies what delimter should be used
-        
+
   *****************************************************************/
-  
-	var file_reader;
-	try {
+
+  var file_reader;
+  try {
     file_reader = new FileReader();
-	} catch(e) {
-		alert("Error: seems File API is not supported on your browser");
+  } catch(e) {
+    alert("Error: seems File API is not supported on your browser");
   }
 
-  file_reader.onload = function(e) { 
-    insert_file(file.fileName, e.target.result, insert_mode, delim_mode) 
+  file_reader.onload = function(e) {
+    insert_file(file.fileName, e.target.result, insert_mode, delim_mode)
   }
-  
+
   file_reader.readAsText(file, "UTF-8");
 }
 
 function insert_file(file_name, content, insert_mode, delim_mode) {
   /*****************************************************************
     <Purpose>
-      Callbackfunction when a file to be inserted is read. 
+      Callbackfunction when a file to be inserted is read.
       It specifies the delimiter, and the place where the file should
-      be inserted. 
-      
+      be inserted.
+
     <Arguments>
       file_name (string)
-      
+
       content (string)
-      
-      insert_mode  (string) 
+
+      insert_mode  (string)
         "begin" or "cursor" - specifies where the file should be inserted
-      
+
       delim_mode (string)
         "lines", "line" or "none" - specifies what delimter should be used
-        
+
     <Side Effects>
       None.
     <Returns>
@@ -495,31 +495,31 @@ function insert_file(file_name, content, insert_mode, delim_mode) {
   // Make Delimiter.
   var delimiter_top = "\n\n";
   var delimiter_bottom = "\n\n";
-  
+
   if (delim_mode == "lines") {
-    delimiter_top += 
-     "#######################################################\n" + 
-     "#######################################################\n" + 
-     "#######################################################\n" + 
-     "#######################################################\n" + 
+    delimiter_top +=
+     "#######################################################\n" +
+     "#######################################################\n" +
+     "#######################################################\n" +
+     "#######################################################\n" +
      "# begin include '" + file_name + "'\n";
-  
-    delimiter_bottom += 
+
+    delimiter_bottom +=
      "# end include " + file_name + "'\n\n";
-  
+
   } else if (delim_mode == "line") {
-      delimiter_top += 
+      delimiter_top +=
        "# begin include '" + file_name + "'\n";
-  
-      delimiter_bottom += 
+
+      delimiter_bottom +=
        "# end include " + file_name + "'\n\n";
     }
-  
-  
+
+
   if (delim_mode == "begin") {
     g_editor.gotoLine(0);
   }
 
   g_editor.insert(delimiter_top + content + delimiter_bottom);
-  
+
 }
