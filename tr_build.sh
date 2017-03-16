fa25fc88004754d63f724aa14df8b23726eebdb0
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-03-16 16:16:54 -0400

    Makes seattle_repy build argument optional
    In case of a local deployment we don't need to build Try RepyV2
    with seattle_repy modules. We only need those in case of a remote
    deployment.

--- tr_build.sh
+++ tr_build.sh
@@ -1,54 +1,47 @@
 #! /bin/bash
+# "Usage: $0 [</path/to/seattle_repy>"]
 
-echo "Building Try Repy..."
-if [ $# -ne 1 -o ! -d "$1" ]
-  then
-    echo "Usage: $0 </path/to/seattle_repy>" >&2
-    exit 1
-fi
 
 if [ -d build ]
   then
-    echo "Removing build directory..."
+    echo "Removing 'build' directory..."
     rm -rf build
 fi
 
 seattle_repy="$1"
 
-echo "Creating new build directory..."
+echo "Creating new 'build' directory..."
 mkdir build
 
-echo "Calling python script to flat out web directory..."
 python tr_convertfiles.py
 
-echo "Copying needed py and r2py file to build directory..."
+echo "Copying Try RepyV2 Python and r2py files to 'build'..."
 cp tr_webcontroller.r2py build
 cp restrictions.tryrepy build
 cp tr_sandbox.r2py build
 cp tr_fileabstraction.r2py build
 cp upper_dot_lower.py build
 
-
-echo "Copying runtime parts..."
-cp "${seattle_repy}/dylink.r2py" build
-cp "${seattle_repy}/base64.r2py" build
-cp "${seattle_repy}/upper_dot_lower.py" build
-cp "${seattle_repy}/httpserver.r2py" build
-cp "${seattle_repy}/librepy.r2py" build
-cp "${seattle_repy}/librepyrunloop.r2py" build
-cp "${seattle_repy}/priority_queue.r2py" build
-cp "${seattle_repy}/librepyfile.r2py" build
-cp "${seattle_repy}/librepyrandom.r2py" build
-cp "${seattle_repy}/librepythread.r2py" build
-cp "${seattle_repy}/librepysocket.r2py" build
-cp "${seattle_repy}/urllib.r2py" build
-cp "${seattle_repy}/urlparse.r2py" build
-cp "${seattle_repy}/uniqueid.r2py" build
-cp "${seattle_repy}/sockettimeout.r2py" build
-cp "${seattle_repy}/random.r2py" build
-cp "${seattle_repy}/math.r2py" build
-cp "${seattle_repy}/httpretrieve.r2py" build
-
-echo "Finished building! You can now run tr_run.sh </path/to/seattle_repy> <serverport>"
+if [ $# -gt 0 ]
+  then
+    echo "Copying RepyV2 modules to 'build' directory..."
+    cp "${1}/dylink.r2py" build
+    cp "${1}/base64.r2py" build
+    cp "${1}/httpserver.r2py" build
+    cp "${1}/librepy.r2py" build
+    cp "${1}/librepyrunloop.r2py" build
+    cp "${1}/priority_queue.r2py" build
+    cp "${1}/librepyfile.r2py" build
+    cp "${1}/librepyrandom.r2py" build
+    cp "${1}/librepythread.r2py" build
+    cp "${1}/librepysocket.r2py" build
+    cp "${1}/urllib.r2py" build
+    cp "${1}/urlparse.r2py" build
+    cp "${1}/uniqueid.r2py" build
+    cp "${1}/sockettimeout.r2py" build
+    cp "${1}/random.r2py" build
+    cp "${1}/math.r2py" build
+    cp "${1}/httpretrieve.r2py" build
+fi
 
 exit 0
