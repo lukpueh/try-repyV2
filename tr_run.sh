237c6296a02d12a7c9ee479ceeffa4d5eabe8df4
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-03-16 14:19:14 -0400

    Replaces RepyV1 preprocessing with RepyV2 import
    In RepyV1 modules where included using `include` and a preprocessor.
    In RepyV2 we dynamically import modules using dylink.r2py.

--- tr_run.sh
+++ tr_run.sh
@@ -17,9 +17,7 @@ echo "Copying files from build to seattle_repy directory..."
 cp build/* $1
 echo "Changing to $1"
 cd $1
-echo "Preprocessing tr_webcontroller.py..."
-python repypp.py tr_webcontroller.py tr_webcontroller.repy
 echo "Executing tr_webcontroller.repy..."
-python repy.py restrictions.tryrepy tr_webcontroller.repy $2
+python repy.py restrictions.tryrepy dylink.r2py tr_webcontroller.r2py $2
 
 exit 0
